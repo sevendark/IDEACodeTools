c9a0f7415a80f00dccf9fdad6766565e4939bf04
Timothy.ji <sevendark@yeah.net>
2018-12-18 18:30:14 +0800

    create system

--- src/main/java/com/sevendark/ai/plugin/AiCoder.java
+++ src/main/java/com/sevendark/ai/plugin/AiCoder.java
@@ -2,9 +2,15 @@ package com.sevendark.ai.plugin;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.search.PsiSearchHelper;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 public class AiCoder extends AnAction {
 
@@ -15,6 +21,20 @@ public class AiCoder extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
         Project project = e.getProject();
-        Messages.showMessageDialog(project, "Hello EventBanker", "Eventbank", Messages.getInformationIcon());
+        if(Objects.isNull(project)){
+            Messages.showMessageDialog(project, "Please select project", "Eventbank", Messages.getInformationIcon());
+            return;
+        }
+        ModuleManager moduleManager = ModuleManager.getInstance(project);
+        Module eb_rest = moduleManager.findModuleByName("eb-rest");
+        if(Objects.isNull(eb_rest)){
+            Messages.showMessageDialog(project, "Can not find eb-rest module", "Eventbank", Messages.getInformationIcon());
+            return;
+        }
+        PsiSearchHelper searchHelper = PsiSearchHelper.getInstance(project);
+        PsiFile[] a = searchHelper.findFilesWithPlainTextWords("import play.libs.F.Option;");
+
+        Messages.showMessageDialog(project, Objects.toString(a.length), "Eventbank", Messages.getInformationIcon());
+        /*Query<PsiReference> a = ReferencesSearch.search();*/
     }
 }
