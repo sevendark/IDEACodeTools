c60faa2babed5411a999d737516da4177cd3f19d
Timothy.ji <sevendark@yeah.net>
2018-12-20 16:31:37 +0800

    add scala support

--- src/main/java/com/sevendark/ai/plugin/AiCoder.java
+++ src/main/java/com/sevendark/ai/plugin/AiCoder.java
@@ -6,13 +6,19 @@ import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.search.PsiSearchHelper;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.search.searches.ReferencesSearch;
+import com.intellij.util.Query;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiManager;
+import scala.Option;
 
 import java.util.Objects;
 
 public class AiCoder extends AnAction {
+
+    private static final String EB_REST_NAME = "untitled";
 
     public AiCoder() {
         super("Eventbank");
@@ -21,20 +27,21 @@ public class AiCoder extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
         Project project = e.getProject();
-        if(Objects.isNull(project)){
-            Messages.showMessageDialog(project, "Please select project", "Eventbank", Messages.getInformationIcon());
-            return;
-        }
-        ModuleManager moduleManager = ModuleManager.getInstance(project);
-        Module eb_rest = moduleManager.findModuleByName("eb-rest");
+        ModuleManager moduleManager = ModuleManager.getInstance(Objects.requireNonNull(project));
+        Module eb_rest = moduleManager.findModuleByName(EB_REST_NAME);
         if(Objects.isNull(eb_rest)){
             Messages.showMessageDialog(project, "Can not find eb-rest module", "Eventbank", Messages.getInformationIcon());
             return;
         }
-        PsiSearchHelper searchHelper = PsiSearchHelper.getInstance(project);
-        PsiFile[] a = searchHelper.findFilesWithPlainTextWords("import play.libs.F.Option;");
 
-        Messages.showMessageDialog(project, Objects.toString(a.length), "Eventbank", Messages.getInformationIcon());
-        /*Query<PsiReference> a = ReferencesSearch.search();*/
+        ScalaPsiManager scalaPsiManager = ScalaPsiManager.instance(project);
+        Option<PsiClass> psiClass = scalaPsiManager.getCachedClass(eb_rest.getModuleScope(), "controllers.HomeController");
+        if(psiClass.isDefined()){
+            Query<PsiReference> a = ReferencesSearch.search(psiClass.get());
+            Messages.showMessageDialog(project, Objects.toString(a.findFirst()), "Eventbank", Messages.getInformationIcon());
+        }else{
+            Messages.showMessageDialog(project, "Can not find play.libs.F.Option", "Eventbank", Messages.getInformationIcon());
+        }
+
     }
 }
