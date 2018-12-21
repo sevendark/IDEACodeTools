1d98a539a0e7510997a803434ade7abcfe959bac
Timothy.ji <sevendark@yeah.net>
2018-12-21 15:00:07 +0800

    find scala

--- src/main/java/com/sevendark/ai/plugin/AiCoder.java
+++ src/main/java/com/sevendark/ai/plugin/AiCoder.java
@@ -8,40 +8,83 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiReference;
+import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.search.searches.ReferencesSearch;
 import com.intellij.util.Query;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.scala.lang.psi.impl.ScalaPsiManager;
 import scala.Option;
 
+import java.util.Arrays;
 import java.util.Objects;
+import java.util.Optional;
+
+import static java.util.Optional.*;
 
 public class AiCoder extends AnAction {
 
     private static final String EB_REST_NAME = "untitled";
+    private static final String EB = "Eventbank";
 
     public AiCoder() {
         super("Eventbank");
     }
 
     @Override
-    public void actionPerformed(@NotNull AnActionEvent e) {
-        Project project = e.getProject();
-        ModuleManager moduleManager = ModuleManager.getInstance(Objects.requireNonNull(project));
-        Module eb_rest = moduleManager.findModuleByName(EB_REST_NAME);
-        if(Objects.isNull(eb_rest)){
-            Messages.showMessageDialog(project, "Can not find eb-rest module", "Eventbank", Messages.getInformationIcon());
+    public void actionPerformed(@NotNull AnActionEvent actionEvent) {
+        Project project = actionEvent.getProject();
+        Module module = getEbRest(project).orElse(null);
+        PsiClass option = getPlayOption(project).orElse(null);
+        if(Objects.isNull(option) || Objects.isNull(module)){
             return;
         }
+        Query<PsiReference> search = ReferencesSearch.search(option, module.getModuleScope());
+        search.forEach(e -> {
+            System.out.println(e.getCanonicalText()); //play.libs.F
+            System.out.println(e.getElement()); // Code Reference Element : play.libs.F
+            System.out.println(e.getRangeInElement()); // (10, 11)
+            System.out.println(Arrays.toString(e.getVariants())); // [Scala, F, Akka, Json, XML, HttpExecution]
+        });
+    }
 
-        ScalaPsiManager scalaPsiManager = ScalaPsiManager.instance(project);
-        Option<PsiClass> psiClass = scalaPsiManager.getCachedClass(eb_rest.getModuleScope(), "controllers.HomeController");
-        if(psiClass.isDefined()){
-            Query<PsiReference> a = ReferencesSearch.search(psiClass.get());
-            Messages.showMessageDialog(project, Objects.toString(a.findFirst()), "Eventbank", Messages.getInformationIcon());
-        }else{
-            Messages.showMessageDialog(project, "Can not find play.libs.F.Option", "Eventbank", Messages.getInformationIcon());
+    private Optional<Module> getEbRest(Project project){
+        if(Objects.isNull(project)){
+            showErrorMsg("Can't find project");
+            return empty();
         }
+        ModuleManager moduleManager = ModuleManager.getInstance(project);
+        Module eb_rest = moduleManager.findModuleByName(EB_REST_NAME);
+        if(Objects.isNull(eb_rest)){
+            showErrorMsg("Can't find " + EB_REST_NAME);
+        }
+        return ofNullable(eb_rest);
+    }
 
+    private Optional<PsiClass> getPlayOption(Project project){
+        Module module = getEbRest(project).orElse(null);
+        if(Objects.isNull(module)){
+            return empty();
+        }
+        GlobalSearchScope searchScope = GlobalSearchScope.moduleWithDependenciesAndLibrariesScope(module);
+        Option<PsiClass> f = ScalaPsiManager.instance(project).getCachedClass(searchScope, "play.libs.F");
+        if(f.isEmpty()){
+            showErrorMsg("Can't find play.libs.F");
+        }
+        PsiClass[] innerClazz = f.get().getInnerClasses();
+        for (PsiClass inner : innerClazz){
+            if(Objects.equals(inner.getQualifiedName(), "play.libs.F.Option")){
+                return of(inner);
+            }
+        }
+        showErrorMsg("Can't find play.libs.F$Option");
+        return empty();
+    }
+
+    private void showMsg(Object msg){
+        Messages.showInfoMessage(Objects.toString(msg), EB);
+    }
+
+    private void showErrorMsg(Object msg){
+        Messages.showErrorDialog(Objects.toString(msg), EB);
     }
 }
