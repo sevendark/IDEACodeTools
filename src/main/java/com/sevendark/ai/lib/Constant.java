87ea59536f2a0e81b88a881c72f46d2c4c203156
Timothy.ji <sevendark@yeah.net>
2019-01-15 16:41:08 +0800

    fix reg

--- src/main/java/com/sevendark/ai/lib/Constant.java
+++ src/main/java/com/sevendark/ai/lib/Constant.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 public interface Constant {
    String GROUP_NAME = "com.sevendark.ai.plugin.GroupedActions";
    String VAR = "(\\w*[a-z]+\\w*\\.?)+";
-   String METHOD = "(([a-zA-Z0-9_]+\\.[a-zA-Z0-9_]+)\\.)?[a-zA-Z_]+\\(?";
+   String METHOD = "(([a-zA-Z0-9_]+)\\.)*[a-zA-Z_]+\\(?";
    List<String> supportLanuage = new ArrayList<String>(){{
       add("JAVA");
       add("Scala");
